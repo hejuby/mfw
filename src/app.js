1c3fa38866904edbaa9673858165f17e57ed4831
Heejun Byeon <hejuby@gmail.com>
2023-10-09 22:46:32 +0900

    Ratio/color input and generate button

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const imageInputForm = document.querySelector("#image-form");
 const imageTag = document.querySelector("#image");
+const ratioColorWrapper = document.querySelector("#ratio-color-wrapper");
+const widthInput = ratioColorWrapper.querySelector("#ratio-width");
+const heightInput = ratioColorWrapper.querySelector("#ratio-height");
+const colorInput = ratioColorWrapper.querySelector("#color");
+const wallpaperGenerateButton = document.querySelector("#wallpaper-generate");
 
 function handleImageSubmit(event) {
   event.preventDefault();
@@ -7,4 +12,9 @@ function handleImageSubmit(event) {
   imageTag.src = URL.createObjectURL(imageFile);
 }
 
+function handleGenerateWallpaper() {
+  console.log(widthInput, heightInput, colorInput);
+}
+
 imageInputForm.addEventListener("submit", handleImageSubmit);
+wallpaperGenerateButton.addEventListener("click", handleGenerateWallpaper);
