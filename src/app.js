c896551f2a16d9fc7982de11219a76914e729433
Heejun Byeon <hejuby@gmail.com>
2023-10-10 17:32:10 +0900

    Updated calcalateNewRatio function

--- src/app.js
+++ src/app.js
@@ -8,12 +8,32 @@ const wallpaperGenerateButton = document.querySelector("#wallpaper-generate");
 
 function handleImageSubmit(event) {
   event.preventDefault();
+  
   const imageFile = event.target[0].files[0];
   imageTag.src = URL.createObjectURL(imageFile);
 }
 
 function handleGenerateWallpaper() {
-  console.log(widthInput, heightInput, colorInput);
+  const newResolution = calcalateNewRatio(Number(widthInput.value), Number(heightInput.value), imageTag.width, imageTag.height);
+  const canvas = document.querySelector("canvas");
+  canvas.width = newResolution[0];
+  canvas.height = newResolution[1];
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(imageTag, 0, 0);
+  imageTag.classList.add("hidden");
+  
+}
+
+function calcalateNewRatio(ratioWidth, ratioHeight, imageWidth, imageHeight) {
+  const ay = ratioWidth * imageHeight;
+  const bx = ratioHeight * imageWidth;
+  if (ay < bx) {
+    return [imageWidth, Math.round(bx/ratioWidth)];
+  } else if (ay > bx) {
+    return [Math.round(ay/ratioHeight), imageHeight];
+  } else {
+    return [imageWidth, imageHeight];
+  }
 }
 
 imageInputForm.addEventListener("submit", handleImageSubmit);
